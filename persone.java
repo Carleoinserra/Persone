ec959f64dc3181f53a5fa0bb05587b59222e82ce
Carleoinserra <inserracarlo@gmail.com>
2026-05-14 12:27:07 +0200

    Add files via upload

---
+++ persone.java
@@ -0,0 +1,20 @@
+
+public class persone {
+	String nome;
+	String dipartimento;
+	public persone(String nome, String dipartimento) {
+		super();
+		this.nome = nome;
+		this.dipartimento = dipartimento;
+	}
+	
+	public void info() {
+		
+		System.out.println("Sono una persona");
+		
+		
+	}
+	
+	
+
+}
